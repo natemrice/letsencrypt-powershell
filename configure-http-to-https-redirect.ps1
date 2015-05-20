8836e2ab4f985f3fc627c8d6225306e3bfc48b3f
Nathan Rice <natemrice@gmail.com>
2015-05-20 14:57:00 -0700

    Started reconfig of IIS for the 403.4 redirect. HttpErrors I'm going to have to read the current configuration and then update only the 403.4 error because of the way the property is written to IIS.

--- configure-http-to-https-redirect.ps1
+++ configure-http-to-https-redirect.ps1
@@ -19,7 +19,7 @@
 #
 # ------------------------------------------------------------------------
 
-# HttpErrors 403.4 C:\Inetpub\wwwtest\403-4.htm
+$4034Page = "403-4.htm"
 
 #ServerState codes
 Function Get-State($State){
@@ -132,15 +132,37 @@ Function SetRequireSSL($SiteID) {
 }
 
 Function ConfigureSSLRedirect($SiteID) {
-	#ToDo
-
 	#Since there is no native way to redirect in IIS 6, I was thinking I could URL redirect
 	#HTTP requests to HTTPS based on the 403 error page that gets returned, via JavaScript.
 	#this method is obviously going to fail on browsers that do not have JavaScript enabled,
 	#maybe someone else can think of a better way.
-	#"<script>window.location = ""https:"" + window.location.href.substring(window.location.protocol.length)</script>"
+	
+	#ToDo: Need to read/write HttpErrors and disable HTTPS requirement on redirect page.
+	$RedirectPage = "<script>window.location = 'https:' + window.location.href.substring(window.location.protocol.length)</script>"
+	
+	If (Get-Command Get-Website -CommandType Cmdlet -errorAction SilentlyContinue) {
+		$WebSites = Get-Website
+	} Else {
+		$WebSites = GetIIS6Websites
+	}
+	
+	If ($WebSites.Count -eq 0) { Exit }
+	
+	ForEach ($WebSite In $WebSites) {
+		If (!(Test-Path "$WebSite.PhysicalPath`\$4034Page")) {
+			#If the redirect page doesn't exist, create it.
+			Try {
+				$RedirectPage | Out-File "$WebSite.PhysicalPath`\$4034Page"
+			} Catch {
+				Write-Error "Failed to create 403.4 redirect page at path: $WebSite.PhysicalPath`\$4034Page"
+			}
+		}
+	}
 }
 
 #SetRequireSSL 87257621
+
+
+
 
 
